21e5174812eacc1625b19d41f7c433a3a218414a
Matteo <7420345+MattePozzy@users.noreply.github.com>
2023-12-21 17:22:47 +0100

    Add files via upload

---
+++ MauiApp6/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Timers;
+
+namespace MauiApp6
+{
+    public class MyClass
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public partial class ViewModel : ObservableObject
+    {
+        private System.Timers.Timer MyTimer = new System.Timers.Timer(5000);
+
+        [ObservableProperty]
+        public List<MyClass> _myList;
+
+        public ViewModel()
+        {
+            LoadList();
+
+            MyTimer.Elapsed += (object sender, ElapsedEventArgs e) =>
+            {
+                LoadList();
+            };
+            MyTimer.Start();
+        }
+
+        public void LoadList()
+        {
+            List<MyClass> list = [];
+            int count = new Random().Next(1, 100);
+
+            for (int i = 0; i < 30; i++)
+            {
+                list.Add(new MyClass
+                {
+                    Name = $"Name_{i}_{count}",
+                    Description = $"Description_{i}_{count}",
+                });
+            }
+
+            MyList = list;
+        }
+    }
+}
